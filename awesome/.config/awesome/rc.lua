3ed007cb1bd3c2791a7b06a4cf310cd26d07079f
Jose <joseoctavioconti@gmail.com>
2022-11-10 13:56:01 -0300

    picom

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -873,6 +873,7 @@ beautiful.useless_gap = 3
 beautiful.notification_icon_size = 120
 beautiful.notification_border_width = 1
 beautiful.notification_border_color = '#F38BA8'
+beautiful.notification_bg = '#1E1E2E'
 
 naughty.config.icon_dirs = { "/usr/share/pixmaps", "/usr/share/icons/Papirus-Dark/48x48/status/",
   "/usr/share/icons/Papirus-Dark/48x48/devices/" }
@@ -880,7 +881,6 @@ naughty.config.icon_formats = { "png", "gif", "svg" }
 awful.spawn.with_shell("xrandr --output DP-0 --rate 144.00 --primary --mode 1920x1080 --pos 1280x0 --rotate normal --output DP-1 --off --output eDP-1-1 --rate 60.02 --mode 1280x720 --pos 0x360 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off --output HDMI-1-2 --off")
 -- awful.spawn.with_shell("xrandr --output eDP-1-1 --pos 0x360 --mode 1280x720 --rate 60.02 --output DP-0 --pos 1280x0 --primary --mode 1920x1080 --rate 144.00 --right-of eDP-1-1")
 awful.spawn("easystroke")
--- awful.spawn.with_shell("picom")
 awful.spawn("flameshot")
 -- awful.spawn.with_shell("blueman-applet")
 -- awful.spawn.with_shell("sudo ethtool --change enp3s0f1 wol g")
--- lvim/.config/lvim/lua/user/autocommands.lua
+++ lvim/.config/lvim/lua/user/autocommands.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd({ "ColorScheme" }, {
     vim.cmd "hi NvimTreeFolderName guibg=none"
     vim.cmd "hi NvimTreeGitDirty guifg=#76cce0"
     vim.cmd "hi NvimTreeGitStaged guibg=#a7df78"
-    vim.cmd "hi NvimTreeOpenedFolderName guifg=#b4b4b4"
+    vim.cmd "hi NvimTreeOpenedFolderName guifg=none"
     vim.cmd "hi NvimTreeSpecialFile gui=bold,underline guifg=#d79921"
   end,
 })
