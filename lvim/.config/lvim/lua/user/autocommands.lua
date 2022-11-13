9aaac9fba1c0d402a19e5e7287bd15c51763d6bb
Jose <joseoctavioconti@gmail.com>
2022-11-13 13:51:01 -0300

    font changes

--- lvim/.config/lvim/lua/user/autocommands.lua
+++ lvim/.config/lvim/lua/user/autocommands.lua
@@ -3,7 +3,8 @@ vim.api.nvim_create_autocmd({ "ColorScheme" }, {
     vim.cmd "hi NvimTreeFolderName guibg=none"
     vim.cmd "hi NvimTreeGitDirty guifg=#76cce0"
     vim.cmd "hi NvimTreeGitStaged guibg=#a7df78"
-    vim.cmd "hi NvimTreeOpenedFolderName guifg=none"
     vim.cmd "hi NvimTreeSpecialFile gui=bold,underline guifg=#d79921"
+    vim.cmd "hi NvimTreeFolderName guifg=#b39df3"
+    vim.cmd "hi NvimTreeOpenedFolderName guifg=#b39df3"
   end,
 })
